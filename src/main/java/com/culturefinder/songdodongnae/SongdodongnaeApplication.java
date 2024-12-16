2a40475389cd821113672cc73ac0798f6d97f206
suee97 <atn1su@gmail.com>
2024-12-17 05:21:25 +0900

    초기 생성 파일 추가

---
+++ src/main/java/com/culturefinder/songdodongnae/SongdodongnaeApplication.java
@@ -0,0 +1,13 @@
+package com.culturefinder.songdodongnae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongdodongnaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongdodongnaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/culturefinder/songdodongnae/SongdodongnaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culturefinder.songdodongnae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongdodongnaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
