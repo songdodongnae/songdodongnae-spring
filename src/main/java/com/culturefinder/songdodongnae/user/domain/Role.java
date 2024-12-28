5b680df06803862e15e2179fe6234c3753abf6a8
suee97 <atn1su@gmail.com>
2024-12-28 17:12:53 +0900

    feat: user entity 추가

---
+++ src/main/java/com/culturefinder/songdodongnae/user/domain/Role.java
@@ -0,0 +1,5 @@
+package com.culturefinder.songdodongnae.user.domain;
+
+public enum Role {
+    ROLE_USER, ROLE_ADMIN
+}
--- src/main/java/com/culturefinder/songdodongnae/user/domain/User.java
+++ src/main/java/com/culturefinder/songdodongnae/user/domain/User.java
@@ -1,4 +1,26 @@
 package com.culturefinder.songdodongnae.user.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
 public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String nickname;
+
+    private String email;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
 }
