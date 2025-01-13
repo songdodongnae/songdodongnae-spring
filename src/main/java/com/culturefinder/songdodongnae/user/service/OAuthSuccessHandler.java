3a89fbc56a51b08916863a1135833409f01ae102
suee97 <atn1su@gmail.com>
2025-01-13 20:53:31 +0900

    test: 로그인 완료 시 localhost로 보내도록 수정

--- src/main/java/com/culturefinder/songdodongnae/user/service/OAuthSuccessHandler.java
+++ src/main/java/com/culturefinder/songdodongnae/user/service/OAuthSuccessHandler.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
@@ -15,5 +16,11 @@ public class OAuthSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         // [ 웹 서버 uri + 토큰 ] 으로 리다이렉트
+
+        // 테스트 코드
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("http://localhost:3000");
+        builder.path("auth");
+        builder.queryParam("accessToken", "abcdefg");
+        response.sendRedirect(builder.build().toString());
     }
 }
