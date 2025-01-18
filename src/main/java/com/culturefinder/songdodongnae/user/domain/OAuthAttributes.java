22c885baa7aff9091925bbb5b46dbe1f96b3d20d
choiyoung69 <choiyoung609@gmail.com>
2025-01-18 13:43:07 +0900

    feat: OauthAttibutes kakao 기능 추가

--- src/main/java/com/culturefinder/songdodongnae/user/domain/OAuthAttributes.java
+++ src/main/java/com/culturefinder/songdodongnae/user/domain/OAuthAttributes.java
@@ -19,7 +19,23 @@ public enum OAuthAttributes {
             (String) attributes.get("name"),
             (String) attributes.get("email"),
             "google"
-    ));
+    )),
+    KAKAO("kakao", (attributes) -> {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        if (kakaoAccount == null) {
+            throw new IllegalStateException("Kakao account error.");
+        }
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+        if (profile == null) {
+            throw new IllegalStateException("Profile error.");
+        }
+        return new UserProfile(
+                String.valueOf(attributes.get("id")),
+                (String) profile.get("nickname"),
+                (String) kakaoAccount.get("email"),
+                "kakao"
+        );
+    });
 
     private final String registrationId;
     private final Function<Map<String, Object>, UserProfile> of;
