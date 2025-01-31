af1619096fa5147cf6cceaf7da8fcd342e0fc14b
Huiyeongkim <huiyeong9619@naver.com>
2025-01-31 22:07:16 +0900

    test: festival update delete read 테스트 코드 작성

--- src/test/java/com/culturefinder/songdodongnae/festival/FestivalRepositoryTest.java
+++ src/test/java/com/culturefinder/songdodongnae/festival/FestivalRepositoryTest.java
@@ -97,5 +97,75 @@ public class FestivalRepositoryTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    @DisplayName("축제 controller update")
+    public void controllerUpdate() {
+
+        Festival savedFestival = festivalRepository.saveFestival(new Festival(
+                null, "축제1", FestivalCategory.FESTIVAL, null, null, null, null, null,
+                "서울...", "200원쯤?", "010.22..", "https:///....", "http://,,,,,", "설명",
+                "한줄설명", new ArrayList<>(), new ArrayList<>()
+        ));
+
+        Festival updatedFestival = new Festival(
+                savedFestival.getId(), "업데이트된 축제", FestivalCategory.FESTIVAL,
+                savedFestival.getStartDate(), savedFestival.getEndDate(), savedFestival.getStartTime(),
+                savedFestival.getEndTime(), savedFestival.getTimeDescription(), "서울 2",
+                "1000원", "010.33.44.44", "http://22,,,", "http://,,,",
+                "업데이트된 설명", "업데이트된 한줄설명", new ArrayList<>(), new ArrayList<>()
+        );
+
+        Festival updatedSavedFestival = festivalRepository.updateFestival(savedFestival.getId(),updatedFestival);
+
+        assertThat(updatedSavedFestival.getName()).isEqualTo("업데이트된 축제");
+        assertThat(updatedSavedFestival.getLocation()).isEqualTo("서울 2");
+        assertThat(updatedSavedFestival.getFee()).isEqualTo("1000원");
+        assertThat(updatedSavedFestival.getContact()).isEqualTo("010.33.44.44");
+        assertThat(updatedSavedFestival.getHomePageUrl()).isEqualTo("http://22,,,");
+        assertThat(updatedSavedFestival.getReservationUrl()).isEqualTo("http://,,,");
+        assertThat(updatedSavedFestival.getDescription()).isEqualTo("업데이트된 설명");
+        assertThat(updatedSavedFestival.getOnelineDescription()).isEqualTo("업데이트된 한줄설명");
+    }
+
+    @Test
+    @DisplayName("축제 controller delete")
+    public void controllerDelete() {
+        Festival savedFestival = festivalRepository.saveFestival(new Festival(
+                null, "축제1", FestivalCategory.FESTIVAL, null, null, null, null, null,
+                "서울...", "200원쯤?", "010.22..", "https:///....", "http://,,,,,", "설명",
+                "한줄설명", new ArrayList<>(), new ArrayList<>()
+        ));
+
+        festivalRepository.deleteFestival(savedFestival.getId());
+
+        assertThat(festivalRepository.findById(savedFestival.getId())).isNull();
+    }
+
+    @Test
+    @DisplayName("축제 controller read")
+    public void controllerRead() {
+        FestivalReqDto festivalReqDto = new FestivalReqDto("축제2", FestivalCategory.FESTIVAL,  LocalDate.of(2025, 5, 1), LocalDate.of(2025, 5, 3),  LocalTime.of(10, 0),
+                LocalTime.of(18, 0), null, "서울...", "200원쯤?", "010.22..", "http://,,,,,", "http://,,,,,", "설명", "한줄설명");
+
+        Festival savedFestival = festivalRepository.saveFestival(festivalReqDto.toEntity());
+        Festival findFestival = festivalRepository.findById(savedFestival.getId());
+
+        assertThat(findFestival.getName()).isEqualTo("축제2");
+        assertThat(findFestival.getLocation()).isEqualTo("서울...");
+        assertThat(findFestival.getCategory()).isEqualTo(FestivalCategory.FESTIVAL);
+        assertThat(findFestival.getStartDate()).isEqualTo(LocalDate.of(2025, 5, 1));
+        assertThat(findFestival.getEndDate()).isEqualTo(LocalDate.of(2025, 5, 3));
+        assertThat(findFestival.getStartTime()).isEqualTo(LocalTime.of(10, 0));
+        assertThat(findFestival.getEndTime()).isEqualTo(LocalTime.of(18, 0));
+        assertThat(findFestival.getTimeDescription()).isEqualTo(null);
+        assertThat(findFestival.getLocation()).isEqualTo("서울...");
+        assertThat(findFestival.getFee()).isEqualTo("200원쯤?");
+        assertThat(findFestival.getContact()).isEqualTo("010.22..");
+        assertThat(findFestival.getHomePageUrl()).isEqualTo("http://,,,,,");
+        assertThat(findFestival.getReservationUrl()).isEqualTo("http://,,,,,");
+        assertThat(findFestival.getDescription()).isEqualTo("설명");
+        assertThat(findFestival.getOnelineDescription()).isEqualTo("한줄설명");
+
+    }
 
 }
