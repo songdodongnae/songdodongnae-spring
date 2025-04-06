1fdff59acc8959192e1f1402c1e8247de7bb6337
suee97 <atn1su@gmail.com>
2025-04-06 13:29:30 +0900

    feat: 시리즈 등록 화면

--- src/main/java/com/culturefinder/songdodongnae/admin/series/controller/AdminSeriesController.java
+++ src/main/java/com/culturefinder/songdodongnae/admin/series/controller/AdminSeriesController.java
@@ -1,14 +1,23 @@
 package com.culturefinder.songdodongnae.admin.series.controller;
 
+import com.culturefinder.songdodongnae.creator.domain.Creator;
+import com.culturefinder.songdodongnae.creator.repository.CreatorRepository;
+import com.culturefinder.songdodongnae.series.domain.SeriesCategory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("admin/series")
 public class AdminSeriesController {
+
+    private final CreatorRepository creatorRepository;
 
     @GetMapping
     public String series_get() {
@@ -16,8 +25,16 @@ public class AdminSeriesController {
     }
 
     @GetMapping("/create")
-    public String series_create_get() {
+    public String series_create_get(Model model) {
+        List<Creator> creatorList = creatorRepository.findAllCreator();
+        List<SeriesCategory> categoryList = List.of(SeriesCategory.values());
+        model.addAttribute("creatorList", creatorList);
+        model.addAttribute("categoryList", categoryList);
         return "admin/series_create";
     }
 
+    @PostMapping("/create")
+    public String series_create_post() {
+        return "redirect:/admin/series";
+    }
 }
