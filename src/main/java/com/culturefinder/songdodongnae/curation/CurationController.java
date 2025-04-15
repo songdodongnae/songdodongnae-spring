28902474d9e14964683bcb217cf108d7949bb9ec
suee97 <atn1su@gmail.com>
2025-04-15 15:04:40 +0900

    fix: swagger 버그 수정

--- src/main/java/com/culturefinder/songdodongnae/curation/CurationController.java
+++ src/main/java/com/culturefinder/songdodongnae/curation/CurationController.java
@@ -5,6 +5,7 @@ import com.culturefinder.songdodongnae.series.domain.Series;
 import com.culturefinder.songdodongnae.series.domain.SeriesDeliciousSpot;
 import com.culturefinder.songdodongnae.series.repository.SeriesRepository;
 import com.culturefinder.songdodongnae.utils.ResponseContainer;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,19 +25,21 @@ public class CurationController {
     private final CurationService curationService;
     private final SeriesRepository seriesRepository;
 
-    @GetMapping("/thumbnail/all")
-    public ResponseEntity<ResponseContainer<List<CurationThumbnailResDto>>> getAllCurationThumbnails() {
-        List<CurationThumbnailResDto> dtos = curationService.getAllCurationThumbnails();
-        return new ResponseContainer<>(HttpStatus.OK, "큐레이션 썸네일 목록 조회 성공", dtos).toResponseEntity();
-    }
+//    @GetMapping("/thumbnail/all")
+//    public ResponseEntity<ResponseContainer<List<CurationThumbnailResDto>>> getAllCurationThumbnails() {
+//        List<CurationThumbnailResDto> dtos = curationService.getAllCurationThumbnails();
+//        return new ResponseContainer<>(HttpStatus.OK, "큐레이션 썸네일 목록 조회 성공", dtos).toResponseEntity();
+//    }
 
     @GetMapping("/thumbnail/top")
+    @Operation(summary = "큐레이션 썸네일 상위 20개 조회", description = "홈화면에 보여지는 큐레이션 썸네일 20개를 조회합니다.")
     public ResponseEntity<ResponseContainer<List<CurationThumbnailResDto>>> getTopCurationThumbnails() {
         List<CurationThumbnailResDto> dtos = curationService.getTopCurationThumbnails();
         return new ResponseContainer<>(HttpStatus.OK, "큐레이션 썸네일 상위 20개 조회 성공", dtos).toResponseEntity();
     }
 
     @GetMapping("/thumbnail")
+    @Operation(summary = "시리즈에 포함된 큐레이션 조회", description = "특정 시리즈에 포함된 큐레이션을 조회합니다. (API 변경 예정)")
     public ResponseEntity<ResponseContainer<List<CurationThumbnailResDto>>> getCurationThumbnailsBySeriesId(
             @RequestParam Long seriesId
     ) {
