7892d6f45f6d2ddb802c2829037bbf013d63d8d5
suee97 <atn1su@gmail.com>
2025-04-15 14:44:33 +0900

    feat: 시리즈에 포함된 큐레이션 목록 조회 api

--- src/main/java/com/culturefinder/songdodongnae/curation/CurationController.java
+++ src/main/java/com/culturefinder/songdodongnae/curation/CurationController.java
@@ -1,21 +1,28 @@
 package com.culturefinder.songdodongnae.curation;
 
+import com.culturefinder.songdodongnae.delicious_spot.domain.DeliciousSpot;
+import com.culturefinder.songdodongnae.series.domain.Series;
+import com.culturefinder.songdodongnae.series.domain.SeriesDeliciousSpot;
+import com.culturefinder.songdodongnae.series.repository.SeriesRepository;
 import com.culturefinder.songdodongnae.utils.ResponseContainer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("/curation")
+@RequestMapping("/api/curation")
 @RestController
 public class CurationController {
 
     private final CurationService curationService;
+    private final SeriesRepository seriesRepository;
 
     @GetMapping("/thumbnail/all")
     public ResponseEntity<ResponseContainer<List<CurationThumbnailResDto>>> getAllCurationThumbnails() {
@@ -27,6 +34,31 @@ public class CurationController {
     public ResponseEntity<ResponseContainer<List<CurationThumbnailResDto>>> getTopCurationThumbnails() {
         List<CurationThumbnailResDto> dtos = curationService.getTopCurationThumbnails();
         return new ResponseContainer<>(HttpStatus.OK, "큐레이션 썸네일 상위 20개 조회 성공", dtos).toResponseEntity();
+    }
+
+    @GetMapping("/thumbnail")
+    public ResponseEntity<ResponseContainer<List<CurationThumbnailResDto>>> getCurationThumbnailsBySeriesId(
+            @RequestParam Long seriesId
+    ) {
+        // TODO: 시리즈-축제 연결 부분이 아직 구현이 안되서 맛집만 보여줌
+        Series series = seriesRepository.findSeriesById(seriesId);
+        List<SeriesDeliciousSpot> seriesDeliciousSpotList = series.getSeriesDeliciousSpotList();
+        List<CurationThumbnailResDto> dtos = new ArrayList<>();
+        for (SeriesDeliciousSpot e: seriesDeliciousSpotList) {
+            DeliciousSpot deliciousSpot = e.getDeliciousSpot();
+            CurationThumbnailResDto dto = CurationThumbnailResDto
+                    .builder()
+                    .id(deliciousSpot.getId())
+                    .title(deliciousSpot.getName())
+                    .imageUrl(deliciousSpot.getImageUrl())
+                    .createdTime(deliciousSpot.getCreatedTime())
+                    .introduction(deliciousSpot.getOnelineDescription())
+                    .curationType(CurationType.DELICIOUS_SPOT)
+                    .build();
+            dtos.add(dto);
+        }
+
+        return new ResponseContainer<>(HttpStatus.OK, "특정 시리즈에 포함된 모든 큐레이션 조회 성공", dtos).toResponseEntity();
     }
 
 }
---
+++ src/main/java/com/culturefinder/songdodongnae/curation/CurationType.java
@@ -0,0 +1,5 @@
+package com.culturefinder.songdodongnae.curation;
+
+public enum CurationType {
+    DELICIOUS_SPOT, FESTIVAL
+}
