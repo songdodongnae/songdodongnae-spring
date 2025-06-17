1fcdb31fe63ec3d8a2ef8c963257fcf5bebe5cde
suee97 <atn1su@gmail.com>
2025-06-17 16:22:17 +0900

    remove: 맛집 관련 테스트 코드 삭제

--- src/test/java/com/culturefinder/songdodongnae/delicious_spot/DeliciousSpotRepositoryTest.java
+++ src/test/java/com/culturefinder/songdodongnae/delicious_spot/DeliciousSpotRepositoryTest.java
@@ -1,153 +1,117 @@
-// TODO: 8
+package com.culturefinder.songdodongnae.delicious_spot;
 
-//package com.culturefinder.songdodongnae.delicious_spot;
-//
-//import com.culturefinder.songdodongnae.delicious_spot.domain.DeliciousSpot;
-//import com.culturefinder.songdodongnae.delicious_spot.domain.DeliciousSpotImage;
-//import com.culturefinder.songdodongnae.delicious_spot.repository.DeliciousSpotRepository;
-//import com.culturefinder.songdodongnae.series.domain.Series;
-//import com.culturefinder.songdodongnae.series.repository.SeriesRepository;
-//import jakarta.persistence.EntityManager;
-//import jakarta.persistence.PersistenceContext;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.assertj.core.api.Assertions.*;
-//
-//
-//@SpringBootTest
-//@Transactional
-//public class DeliciousSpotRepositoryTest {
-//
-//    @Autowired private DeliciousSpotRepository deliciousSpotRepository;
-//    @PersistenceContext private EntityManager em;
-//    @Autowired private SeriesRepository seriesRepository;
-//
-//    @Test
-//    @DisplayName("맛집 리스트 저장되는지 확인하는 테스트")
-//    public void test1() {
-//        DeliciousSpotImage deliciousSpotImage1 = new DeliciousSpotImage("imageUrl1");
-//        DeliciousSpotImage deliciousSpotImage2 = new DeliciousSpotImage("imageUrl2");
-//        DeliciousSpot deliciousSpot = new DeliciousSpot();
-//        List<DeliciousSpotImage> deliciousSpotImageArrayList = new ArrayList<>();
-//        deliciousSpotImageArrayList.add(deliciousSpotImage1);
-//        deliciousSpotImageArrayList.add(deliciousSpotImage2);
-//        deliciousSpot.setDeliciousSpotImages(deliciousSpotImageArrayList);
-//        deliciousSpotImage1.setDeliciousSpot(deliciousSpot);
-//        deliciousSpotImage2.setDeliciousSpot(deliciousSpot);
-//
-//        List<DeliciousSpot> deliciousSpotArrayList = new ArrayList<>();
-//        deliciousSpotArrayList.add(deliciousSpot);
-//        Series deliciousSpotList = new Series();
-//        seriesRepository.addSeries(deliciousSpotList);
-//        deliciousSpot.setSeries(deliciousSpotList);
-//        deliciousSpotRepository.addDeliciousSpot(deliciousSpotList.getId(), deliciousSpot);
-//
-//        Series findDeliciousSpotList = seriesRepository.addSeries(deliciousSpotList);
-//        assertThat(deliciousSpotList.getId()).isEqualTo(findDeliciousSpotList.getId());
-//    }
-//
-//    @Test
-//    @DisplayName("맛집리스트의 맛집을 모두 불러오는지 확인하는 테스트")
-//    public void test2() {
-//        DeliciousSpot deliciousSpot1 = new DeliciousSpot();
-//        DeliciousSpot deliciousSpot2 = new DeliciousSpot();
-//        DeliciousSpot deliciousSpot3 = new DeliciousSpot();
-//
-//        Series deliciousSpotList = new Series();
-//        seriesRepository.addSeries(deliciousSpotList);
-//
-//        deliciousSpot1.setSeries(deliciousSpotList);
-//        deliciousSpot2.setSeries(deliciousSpotList);
-//        deliciousSpot3.setSeries(deliciousSpotList);
-//
-//        deliciousSpotRepository.addDeliciousSpot(deliciousSpotList.getId(), deliciousSpot1);
-//        deliciousSpotRepository.addDeliciousSpot(deliciousSpotList.getId(), deliciousSpot2);
-//        deliciousSpotRepository.addDeliciousSpot(deliciousSpotList.getId(), deliciousSpot3);
-//
-//        List<DeliciousSpot> findDeliciousSpotList = deliciousSpotRepository.findBySeries(deliciousSpotList);
-//
-//        assertThat(findDeliciousSpotList.size()).isEqualTo(3);
-//        assertThat(findDeliciousSpotList.get(0).getSeries().getId()).isEqualTo(deliciousSpotList.getId());
-//        assertThat(findDeliciousSpotList.get(1).getSeries().getId()).isEqualTo(deliciousSpotList.getId());
-//        assertThat(findDeliciousSpotList.get(2).getSeries().getId()).isEqualTo(deliciousSpotList.getId());
-//    }
-//
-//    @Test
-//    @DisplayName("맛집 리스트를 불러오는지 확인하는 테스트")
-//    public void test3() {
-//        Series deliciousSpotList = new Series();
-//        seriesRepository.addSeries(deliciousSpotList);
-//        Series findDeliciousSpotList = seriesRepository.findSeriesById(deliciousSpotList.getId());
-//        assertThat(findDeliciousSpotList.getId()).isEqualTo(deliciousSpotList.getId());
-//    }
-//
-//    @Test
-//    @DisplayName("맛집 리스트를 모두 불러오는지 확인하는 테스트")
-//    public void test4 () {
-//        List<Series> findDeliciousSpotList1 = seriesRepository.findAllSeries();
-//        assertThat(findDeliciousSpotList1.size()).isEqualTo(0);
-//
-//        Series deliciousSpotList = new Series();
-//        seriesRepository.addSeries(deliciousSpotList);
-//        List<Series> findDeliciousSpotList2 = seriesRepository.findAllSeries();
-//        assertThat(findDeliciousSpotList2.size()).isEqualTo(1);
-//    }
-//
-//    @Test
-//    @DisplayName("특정 맛집 리스트에 맛집이 등록되는지 확인하는 테스트")
-//    public void test5() {
-//        Series deliciousSpotList = new Series();
-//        seriesRepository.addSeries(deliciousSpotList);
-//        deliciousSpotRepository.addDeliciousSpot(deliciousSpotList.getId(), new DeliciousSpot());
-//
-//        Series findDeliciousSpotList = seriesRepository.findSeriesById(deliciousSpotList.getId());
-//        assertThat(findDeliciousSpotList).isNotNull();
-//        assertThat(findDeliciousSpotList.getId()).isEqualTo(deliciousSpotList.getId());
-//        assertThat(deliciousSpotRepository.findBySeries(deliciousSpotList).size()).isEqualTo(1);
-//    }
-//
-//    @Test
-//    @DisplayName("특정 맛집 리스트가 지워지는지 확인하는 테스트")
-//    public void test6() {
-//        Series deliciousSpotList = new Series();
-//        seriesRepository.addSeries(deliciousSpotList);
-//        em.flush();
-//        Long deliciousSpotListId = deliciousSpotList.getId();
-//
-//        Series removeDeliciousSpotList = seriesRepository.removeSeries(deliciousSpotListId);
-//        assertThat(removeDeliciousSpotList.getId()).isEqualTo(deliciousSpotListId);
-//    }
-//
-//    @Test
-//    @DisplayName("특정 맛집 리스트가 지워질때 연관된 데이터가 모두 지워지는지 확인하는 테스트")
-//    public void test7() {
-////        Series deliciousSpotList = new Series();
-////        DeliciousSpot deliciousSpot = new DeliciousSpot();
-////        DeliciousSpotImage deliciousSpotImage = new DeliciousSpotImage();
-////        deliciousSpotImage.setDeliciousSpot(deliciousSpot);
-////        deliciousSpot.setDeliciousSpotImages(List.of(deliciousSpotImage));
-////        deliciousSpot.setSeries(deliciousSpotList);
-////        deliciousSpotList.setDeliciousSpots(List.of(deliciousSpot));
-////        Series AddDeliciousSpotList = seriesRepository.addSeries(deliciousSpotList);
-////        em.flush();
-////
-////        Long id = AddDeliciousSpotList.getId();
-////        seriesRepository.removeSeries(id);
-////        em.flush();
-////
-////        List<Series> listlist = em.createQuery("select d from Series d").getResultList();
-////        List<DeliciousSpot> list = em.createQuery("select d from DeliciousSpot d").getResultList();
-////        List<DeliciousSpotImage> image = em.createQuery("select d from DeliciousSpotImage d").getResultList();
-////
-////        assertThat(listlist).isEmpty();
-////        assertThat(list).isEmpty();
-////        assertThat(image).isEmpty();
-//    }
-//}
+import com.culturefinder.songdodongnae.delicious_spot.domain.DeliciousSpot;
+import com.culturefinder.songdodongnae.delicious_spot.repository.DeliciousSpotRepository;
+import com.culturefinder.songdodongnae.exception.CustomException;
+import com.culturefinder.songdodongnae.exception.ErrorCode;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+class DeliciousSpotRepositoryTest {
+
+    @Autowired
+    private EntityManager em;
+
+    private DeliciousSpotRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository = new DeliciousSpotRepository(em);
+    }
+
+    DeliciousSpot createSpot(String name) {
+        DeliciousSpot spot = DeliciousSpot.builder()
+                .name(name)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+        return spot;
+    }
+
+    @Test
+    @DisplayName("맛집 저장 및 조회")
+    void saveAndFindById() {
+        DeliciousSpot spot = createSpot("테스트맛집");
+        repository.saveDeliciousSpot(spot);
+
+        DeliciousSpot found = repository.findDeliciousSpotById(spot.getId());
+        assertThat(found.getName()).isEqualTo("테스트맛집");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 ID 조회시 예외 발생")
+    void findByIdNotFound() {
+        assertThatThrownBy(() -> repository.findDeliciousSpotById(999L))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining(ErrorCode.RESOURCE_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    @DisplayName("최신순 20개 조회")
+    void findTopByOrderByCreatedTimeDesc() {
+        for (int i = 0; i < 25; i++) {
+            repository.saveDeliciousSpot(createSpot("맛집" + i));
+        }
+        List<DeliciousSpot> list = repository.findTopByOrderByCreatedTimeDesc();
+        assertThat(list).hasSize(20);
+        assertThat(list.get(0).getCreatedAt()).isAfterOrEqualTo(list.get(19).getCreatedAt());
+    }
+
+    @Test
+    @DisplayName("전체 조회")
+    void findAll() {
+        repository.saveDeliciousSpot(createSpot("A"));
+        repository.saveDeliciousSpot(createSpot("B"));
+        List<DeliciousSpot> all = repository.findAll();
+        assertThat(all).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("맛집 정보 수정")
+    void updateDeliciousSpot() {
+        DeliciousSpot spot = repository.saveDeliciousSpot(createSpot("수정전"));
+        DeliciousSpot update = createSpot("수정후");
+        DeliciousSpot updated = repository.updateDeliciousSpot(spot.getId(), update);
+
+        assertThat(updated.getName()).isEqualTo("수정후");
+        assertThat(updated.getUpdatedAt()).isAfter(spot.getCreatedAt());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 맛집 수정시 예외 발생")
+    void updateNotFound() {
+        DeliciousSpot update = createSpot("수정");
+        assertThatThrownBy(() -> repository.updateDeliciousSpot(999L, update))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining(ErrorCode.RESOURCE_NOT_FOUND.getMessage());
+    }
+
+    @Test
+    @DisplayName("맛집 삭제")
+    void deleteDeliciousSpot() {
+        DeliciousSpot spot = repository.saveDeliciousSpot(createSpot("삭제대상"));
+        repository.deleteDeliciousSpot(spot.getId());
+
+        assertThatThrownBy(() -> repository.findDeliciousSpotById(spot.getId()))
+                .isInstanceOf(CustomException.class);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 맛집 삭제시 예외 발생")
+    void deleteNotFound() {
+        assertThatThrownBy(() -> repository.deleteDeliciousSpot(999L))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining(ErrorCode.RESOURCE_NOT_FOUND.getMessage());
+    }
+}
