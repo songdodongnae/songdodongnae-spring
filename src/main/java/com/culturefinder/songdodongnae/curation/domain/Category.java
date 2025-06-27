fe52e58bdafb41275d308597a252f25e51142cca
suee97 <atn1su@gmail.com>
2025-06-27 14:25:44 +0900

    remove: 큐레이션 삭제

--- src/main/java/com/culturefinder/songdodongnae/curation/domain/Category.java
+++
@@ -1,5 +0,0 @@
-package com.culturefinder.songdodongnae.curation.domain;
-
-public enum Category {
-    DELICIOUS_SPOT, CAFE, LOCAL_NEIGHBORHOOD, THEMED_BOOK, COURSE_RECOMMENDATION, EVENT_RECOMMENDATION;
-}
--- src/main/java/com/culturefinder/songdodongnae/curation/domain/CurationType.java
+++
@@ -1,5 +0,0 @@
-package com.culturefinder.songdodongnae.curation.domain;
-
-public enum CurationType {
-    DELICIOUS_SPOT, FESTIVAL
-}
