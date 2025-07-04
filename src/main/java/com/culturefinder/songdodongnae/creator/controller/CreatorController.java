4356d6f2c541a7bee9be40a2c3c3f5532048b605
Huiyeongkim <huiyeong9619@naver.com>
2025-07-04 17:01:26 +0900

    feat: 스웨거 적용

--- src/main/java/com/culturefinder/songdodongnae/creator/controller/CreatorController.java
+++ src/main/java/com/culturefinder/songdodongnae/creator/controller/CreatorController.java
@@ -5,6 +5,9 @@ import com.culturefinder.songdodongnae.creator.dto.CreatorResDto;
 import com.culturefinder.songdodongnae.creator.dto.CreatorThumbnailResDto;
 import com.culturefinder.songdodongnae.creator.service.CreatorService;
 import com.culturefinder.songdodongnae.utils.ResponseContainer;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Creators API", description = "크리에이터 관련 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/creators")
 @RestController
@@ -20,30 +24,40 @@ public class CreatorController {
 
     private final CreatorService creatorService;
 
+    @Operation(summary = "크리에이터 생성", description = "크리에이터를 생성합니다.")
+    @ApiResponse(responseCode = "201", description = "크리에이터 생성 성공")
     @PostMapping
     public ResponseEntity<ResponseContainer<CreatorResDto>> createCreator(@Valid @RequestBody CreatorReqDto creatorReqDto) {
         CreatorResDto dto = creatorService.createCreator(creatorReqDto);
         return new ResponseContainer<>(HttpStatus.OK, "크리에이터 생성 성공", dto).toResponseEntity();
     }
 
+    @Operation(summary = "크리에이터 모두 조회", description = "크리에이터 썸네일 목록 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "크리에이터 썸네일 목록 조회 성공")
     @GetMapping
     public ResponseEntity<ResponseContainer<List<CreatorThumbnailResDto>>> getAllCreator() {
         List<CreatorThumbnailResDto> dtos = creatorService.getAllCreator();
         return new ResponseContainer<>(HttpStatus.OK, "크리에이터 썸네일 목록 조회 성공", dtos).toResponseEntity();
     }
 
+    @Operation(summary = "크리에이터 상세 조회", description = "크리에이터 상세 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "크리에이터 상세 조회 성공")
     @GetMapping("/{id}")
     public ResponseEntity<ResponseContainer<CreatorResDto>> getCreator(@PathVariable Long id) {
         CreatorResDto dto = creatorService.getCreator(id);
-        return new ResponseContainer<>(HttpStatus.OK, "크리에이터 조회 성공", dto).toResponseEntity();
+        return new ResponseContainer<>(HttpStatus.OK, "크리에이터 상세 조회 성공", dto).toResponseEntity();
     }
 
+    @Operation(summary = "크리에이터 수정", description = "크리에이터 수정합니다.")
+    @ApiResponse(responseCode = "200", description = "크리에이터 수정 성공")
     @PutMapping("/{id}")
     public ResponseEntity<ResponseContainer<CreatorResDto>> updateCreator(@PathVariable Long id,@Valid @RequestBody CreatorReqDto creatorReqDto) {
         CreatorResDto dto = creatorService.updateCreator(id, creatorReqDto);
         return new ResponseContainer<>(HttpStatus.OK, "크리에이터 수정 성공", dto).toResponseEntity();
     }
 
+    @Operation(summary = "크리에이터 삭제", description = "크리에이터 삭제합니다.")
+    @ApiResponse(responseCode = "200", description = "크리에이터 삭제 성공")
     @DeleteMapping("/{id}")
     public ResponseEntity<ResponseContainer<CreatorResDto>> deleteCreator(@PathVariable Long id) {
         CreatorResDto dto = creatorService.deleteCreator(id);
