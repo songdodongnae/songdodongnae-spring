3a44f860b24fc69a88842a62e723cca69e5ababc
suee97 <atn1su@gmail.com>
2025-07-05 15:59:31 +0900

    feat: 북마크 도메인

--- src/main/java/com/culturefinder/songdodongnae/bookmark/domain/Bookmark.java
+++ src/main/java/com/culturefinder/songdodongnae/bookmark/domain/Bookmark.java
@@ -16,9 +16,9 @@ public class Bookmark {
     @JoinColumn(name = "user_id")
     private User user;
 
-    /*
-    축제인지 맛집인지 큐레이션인지를 구분하는 타입 필요
-    축제, 맛집, 큐레이션이랑 연결되어 있어야 함
-     */
+    @Enumerated(EnumType.STRING)
+    private BookmarkType bookmarkType;
+
+    private Long targetId;
 
 }
