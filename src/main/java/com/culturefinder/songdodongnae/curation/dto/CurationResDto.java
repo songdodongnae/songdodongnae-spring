b28b508723988b79689d749a8d8328f8becfec55
suee97 <atn1su@gmail.com>
2025-07-21 23:02:50 +0900

    refactor: response->res 네이밍 변경

--- src/main/java/com/culturefinder/songdodongnae/curation/dto/CurationResDto.java
+++ src/main/java/com/culturefinder/songdodongnae/curation/dto/CurationResDto.java
@@ -2,19 +2,18 @@ package com.culturefinder.songdodongnae.curation.dto;
 
 import com.culturefinder.songdodongnae.creator.dto.CreatorResDto;
 import com.culturefinder.songdodongnae.curation.domain.Curation;
-import com.culturefinder.songdodongnae.delicious_spot.dto.DeliciousSpotResponseDto;
+import com.culturefinder.songdodongnae.delicious_spot.dto.DeliciousSpotResDto;
 import com.culturefinder.songdodongnae.festival.dto.FestivalResDto;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Builder
 public class CurationResDto {
     private Long id;
     private Integer type;
-    private List<DeliciousSpotResponseDto> deliciousSpots;
+    private List<DeliciousSpotResDto> deliciousSpots;
     private List<FestivalResDto> festivals;
     private CreatorResDto creator;
     private LocalDateTime createdAt;
@@ -24,8 +23,8 @@ public class CurationResDto {
     private String imageUrl;
 
     public static CurationResDto fromEntity(Curation curation) {
-        List<DeliciousSpotResponseDto> deliciousSpotDto = curation.getDeliciousSpots().stream()
-                .map(DeliciousSpotResponseDto::fromEntity)
+        List<DeliciousSpotResDto> deliciousSpotDto = curation.getDeliciousSpots().stream()
+                .map(DeliciousSpotResDto::fromEntity)
                 .toList();
 
         List<FestivalResDto> festivalDto = curation.getFestivals().stream()
