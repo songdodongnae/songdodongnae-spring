d19b270f91ad8fd0218831bbbf4c1fd5623589e9
Huiyeongkim <huiyeong9619@naver.com>
2025-07-29 10:44:18 +0900

    feat: 중간테이블 생성

---
+++ src/main/java/com/culturefinder/songdodongnae/curation/domain/CurationDeliciousSpot.java
@@ -0,0 +1,28 @@
+package com.culturefinder.songdodongnae.curation.domain;
+
+import com.culturefinder.songdodongnae.delicious_spot.domain.DeliciousSpot;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@Entity
+public class CurationDeliciousSpot {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "curation_id")
+    private Curation curation;
+
+    @ManyToOne
+    @JoinColumn(name = "delicious_spot_id")
+    private DeliciousSpot deliciousSpot;
+
+    public CurationDeliciousSpot(Curation curation, DeliciousSpot deliciousSpot) {
+        this.curation = curation;
+        this.deliciousSpot = deliciousSpot;
+    }
+}
+
---
+++ src/main/java/com/culturefinder/songdodongnae/curation/domain/CurationFestival.java
@@ -0,0 +1,27 @@
+package com.culturefinder.songdodongnae.curation.domain;
+
+import com.culturefinder.songdodongnae.festival.domain.Festival;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+public class CurationFestival {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "curation_id")
+    private Curation curation;
+
+    @ManyToOne
+    @JoinColumn(name = "festival_id")
+    private Festival festival;
+
+    public CurationFestival(Curation curation, Festival festival) {
+        this.curation = curation;
+        this.festival = festival;
+    }
+
+}
