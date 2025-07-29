fb031f3aff849f74308a6dacb23ad0066c9552b3
Huiyeongkim <huiyeong9619@naver.com>
2025-07-29 15:58:29 +0900

    feat: 크리에이터 상세조회할 떄 관련 글 응답하도록 수정

--- src/main/java/com/culturefinder/songdodongnae/creator/dto/CreatorResDto.java
+++ src/main/java/com/culturefinder/songdodongnae/creator/dto/CreatorResDto.java
@@ -1,10 +1,13 @@
 package com.culturefinder.songdodongnae.creator.dto;
 
 import com.culturefinder.songdodongnae.creator.domain.Creator;
+import com.culturefinder.songdodongnae.curation.dto.CurationThumbnailResDto;
+import com.culturefinder.songdodongnae.delicious_spot.dto.DeliciousSpotThumbnailResDto;
+import com.culturefinder.songdodongnae.festival.dto.FestivalThumbnailResDto;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Builder
@@ -20,6 +23,12 @@ public class CreatorResDto {
 
     private String imageUrl;
 
+    private List<DeliciousSpotThumbnailResDto> deliciousSpotThumbnails;
+
+    private List<FestivalThumbnailResDto> festivalThumbnails;
+
+    private List<CurationThumbnailResDto> curationThumbnails;
+
     public static CreatorResDto fromEntity(Creator creator) {
         return CreatorResDto.builder()
                 .id(creator.getId())
@@ -27,6 +36,19 @@ public class CreatorResDto {
                 .introduction(creator.getIntroduction())
                 .description(creator.getDescription())
                 .imageUrl(creator.getImageUrl())
+                .build();
+    }
+
+    public static CreatorResDto fromEntity(Creator creator, List<DeliciousSpotThumbnailResDto> deliciousSpotThumbnails, List<FestivalThumbnailResDto> festivalThumbnails, List<CurationThumbnailResDto> curationThumbnails) {
+        return CreatorResDto.builder()
+                .id(creator.getId())
+                .name(creator.getName())
+                .introduction(creator.getIntroduction())
+                .description(creator.getDescription())
+                .imageUrl(creator.getImageUrl())
+                .deliciousSpotThumbnails(deliciousSpotThumbnails)
+                .festivalThumbnails(festivalThumbnails)
+                .curationThumbnails(curationThumbnails)
                 .build();
     }
 }
