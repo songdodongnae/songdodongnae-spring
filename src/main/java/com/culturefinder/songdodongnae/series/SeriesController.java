189f18b07cadf679bebb66f05bbd6ad37af2f6de
Huiyeongkim <huiyeong9619@naver.com>
2025-08-14 01:23:35 +0900

    feat: 시리즈 컨트롤러에 스웨거 추가

--- src/main/java/com/culturefinder/songdodongnae/series/SeriesController.java
+++ src/main/java/com/culturefinder/songdodongnae/series/SeriesController.java
@@ -11,12 +11,16 @@ import com.culturefinder.songdodongnae.user.service.AuthService;
 import com.culturefinder.songdodongnae.utils.CustomPage;
 import com.culturefinder.songdodongnae.utils.ResponseContainer;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+@Tag(name = "Series API", description = "시리즈 관련 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/series")
@@ -27,12 +31,12 @@ public class SeriesController {
     private final FestivalService festivalService;
     private final CurationService curationService;
 
-    @Operation(summary = "모든 맛집 조회", description = "모든 맛집을 조회합니다")
+    @Operation(summary = "모든 맛집 조회")
     @ApiResponse(responseCode = "200", description = "모든 맛집 조회 성공")
     @GetMapping("/delicious-spot")
     public ResponseEntity<ResponseContainer<CustomPage<DeliciousSpotThumbnailResDto>>> getAllDeliciousSpots(
-            @RequestParam(defaultValue = "1") int currentPage,
-            @RequestParam(defaultValue = "10") int pageSize
+            @Parameter(description = "현재 페이지 번호", example = "1") @RequestParam(defaultValue = "1") int currentPage,
+            @Parameter(description = "페이지 크기", example = "10") @RequestParam(defaultValue = "10") int pageSize
     ) {
         if (!authService.isAuthenticatedUser()) {
             CustomPage<DeliciousSpotThumbnailResDto> deliciousSpots = deliciousSpotService.getAllDeliciousSpots(currentPage, pageSize);
@@ -44,12 +48,12 @@ public class SeriesController {
         }
     }
 
-    @Operation(summary = "모든 축제 조회", description = "등록된 모든 축제 목록을 조회합니다.")
+    @Operation(summary = "모든 축제 조회")
     @ApiResponse(responseCode = "200", description = "모든 축제 조회 성공")
     @GetMapping("/festival")
     public ResponseEntity<ResponseContainer<CustomPage<FestivalThumbnailResDto>>> getAllFestivals(
-            @RequestParam(defaultValue = "1") int currentPage,
-            @RequestParam(defaultValue = "10") int pageSize) {
+            @Parameter(description = "현재 페이지 번호", example = "1") @RequestParam(defaultValue = "1") int currentPage,
+            @Parameter(description = "페이지 크기", example = "10") @RequestParam(defaultValue = "10") int pageSize) {
         if (!authService.isAuthenticatedUser()) {
             CustomPage<FestivalThumbnailResDto> dtos = festivalService.getAllFestival(currentPage, pageSize);
             return ResponseContainer.create(HttpStatus.OK, "모든 축제 조회 성공", dtos);
@@ -60,13 +64,13 @@ public class SeriesController {
         }
     }
 
-    @Operation(summary = "타입별로 모든 큐레이션 조회", description = "등록된 타입별로 모든 큐레이션 목록을 조회합니다.")
+    @Operation(summary = "타입별로 모든 큐레이션 조회")
     @ApiResponse(responseCode = "200", description = "타입별로 모든 큐레이션 조회 성공")
     @GetMapping("/curation/{type}")
     public ResponseEntity<ResponseContainer<CustomPage<CurationThumbnailResDto>>> getAllCurationFestival(
-            @PathVariable CurationType type,
-            @RequestParam(defaultValue = "1") int currentPage,
-            @RequestParam(defaultValue = "10") int pageSize) {
+            @Parameter(description = "큐래이션 타입", required = true) @PathVariable CurationType type,
+            @Parameter(description = "현재 페이지 번호", example = "1") @RequestParam(defaultValue = "1") int currentPage,
+            @Parameter(description = "페이지 크기", example = "10") @RequestParam(defaultValue = "10") int pageSize) {
         if (!authService.isAuthenticatedUser()) {
             CustomPage<CurationThumbnailResDto> dtos = curationService.getAllCurationByType(currentPage, pageSize, type);
             return ResponseContainer.create(HttpStatus.OK, "모든 축제 큐레이션 조회 성공", dtos);
