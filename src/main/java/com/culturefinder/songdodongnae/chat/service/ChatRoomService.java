face65e5b41715117c0ea9d7b0416966d649a92d
Huiyeongkim <huiyeong9619@naver.com>
2025-10-02 20:51:14 +0900

    feat: chatroom 생성 기본 틀 구현

--- src/main/java/com/culturefinder/songdodongnae/chat/service/ChatRoomService.java
+++ src/main/java/com/culturefinder/songdodongnae/chat/service/ChatRoomService.java
@@ -1,7 +1,17 @@
 package com.culturefinder.songdodongnae.chat.service;
 
+import com.culturefinder.songdodongnae.chat.dto.ChatRoomResDto;
+import com.culturefinder.songdodongnae.chat.repository.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+
+    public ChatRoomResDto createChatRoom(Long userId, Long boardId) {
+        return null;
+    }
 }
