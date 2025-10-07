f5d68be4f1ab2fce111a619fa352301bb6f7ef0b
Huiyeongkim <huiyeong9619@naver.com>
2025-10-07 18:26:15 +0900

    refactor: 로그인 X

--- src/main/java/com/culturefinder/songdodongnae/user/service/AuthService.java
+++ src/main/java/com/culturefinder/songdodongnae/user/service/AuthService.java
@@ -1,27 +1,28 @@
 package com.culturefinder.songdodongnae.user.service;
 
+import com.culturefinder.songdodongnae.common.exception.CustomException;
+import com.culturefinder.songdodongnae.common.exception.ErrorCode;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthService {
     public Long getAuthenticatedUserId() {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        boolean isAuthenticatedUser = authentication != null &&
-//                authentication.isAuthenticated() &&
-//                !(authentication.getPrincipal() instanceof String &&
-//                        authentication.getPrincipal().equals("anonymousUser"));
-//        if(!isAuthenticatedUser) throw new CustomException(ErrorCode.FORBIDDEN);
-//        return Long.parseLong(authentication.getName());
-        return 3L;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean isAuthenticatedUser = authentication != null &&
+                authentication.isAuthenticated() &&
+                !(authentication.getPrincipal() instanceof String &&
+                        authentication.getPrincipal().equals("anonymousUser"));
+        if(!isAuthenticatedUser) throw new CustomException(ErrorCode.FORBIDDEN);
+        return Long.parseLong(authentication.getName());
     }
 
     public boolean isAuthenticatedUser() {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        return authentication != null &&
-//                authentication.isAuthenticated() &&
-//                !(authentication.getPrincipal() instanceof String &&
-//                        authentication.getPrincipal().equals("anonymousUser"));
-
-        return true;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null &&
+                authentication.isAuthenticated() &&
+                !(authentication.getPrincipal() instanceof String &&
+                        authentication.getPrincipal().equals("anonymousUser"));
     }
 }
